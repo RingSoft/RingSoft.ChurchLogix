2a5886983a1ffba9ada4dc6a80882340bbdae70e
Peter Ringering <peteman316@gmail.com>
2024-12-23 13:40:25 -0700

    Working

--- RingSoft.ChurchLogix/NewDbProcessorFactory.cs
+++ RingSoft.ChurchLogix/NewDbProcessorFactory.cs
@@ -1,12 +1,20 @@
-﻿using RingSoft.DbLookup.Controls.WPF;
+﻿using System.Windows.Controls;
+using RingSoft.DbLookup.Controls.WPF;
+using RingSoft.DbMaintenance;
 
 namespace RingSoft.ChurchLogix
 {
-    internal class NewDbProcessorFactory : DbMaintenanceProcessorFactory
-    {
-        public override DbMaintenanceWindowProcessor GetProcessor()
-        {
-            return new NewDbMaintProcessor1();
-        }
-    }
+    //internal class NewDbProcessorFactory : DbMaintenanceProcessorFactory
+    //{
+    //    public override DbMaintenanceWindowProcessor GetProcessor()
+    //    {
+    //        return new NewDbMaintProcessor1();
+    //    }
+
+    //    public override DbMaintenanceUserControlProcessor GetUserControlProcessor(DbMaintenanceViewModelBase viewModel, Control buttonsControl,
+    //        DbMaintenanceUserControl userControl, DbMaintenanceStatusBar statusBar, IUserControlHost host)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
